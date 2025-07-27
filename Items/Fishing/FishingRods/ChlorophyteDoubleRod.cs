23b038ff364906b7963f0a9670376ec25c4a37af
Armarilloo <drekmarshal@gmail.com>
2025-07-27 18:57:23 -0400

    Reformat; Change DoubleRod to use base class

--- Items/Fishing/FishingRods/ChlorophyteDoubleRod.cs
+++ Items/Fishing/FishingRods/ChlorophyteDoubleRod.cs
@@ -1,51 +1,18 @@
 using MoreFishingRods.Projectiles.Typeless;
-using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace MoreFishingRods.Items.Fishing.FishingRods
 {
-    public class ChlorophyteDoubleRod : ModItem, ILocalizedModType
+    public class ChlorophyteDoubleRod : BaseModFishingRod
     {
-        public new string LocalizationCategory => "Items.Fishing.FishingRods";
-        public override void SetDefaults()
-        {
-            Item.width = 48;
-            Item.height = 48;
-            Item.useAnimation = 8;
-            Item.useTime = 8;
-            Item.useStyle = ItemUseStyleID.Swing;
-            Item.UseSound = SoundID.Item1;
-            Item.fishingPole = 40;
-            Item.shootSpeed = 16f;
-            Item.shoot = ModContent.ProjectileType<ChlorophyteBobber>();
-            Item.value = 37500;
-            Item.rare = ItemRarityID.Lime;
-        }
-
-        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
-        {
-            for (int i = 0; i < 2; i++)
-            {
-                Projectile.NewProjectile(source, position, velocity.RotatedByRandom(MathHelper.ToRadians(18f)), type, 0, 0f, player.whoAmI);
-            }
-            return false;
-        }
-
-        public override void ModifyFishingLine(Projectile bobber, ref Vector2 lineOriginOffset, ref Color lineColor)
-        {
-            lineOriginOffset = new Vector2(47f, -33f);
-            lineColor = new Color(234, 254, 126, 1);
-        }
-
-        public override void AddRecipes()
-        {
-            CreateRecipe().
-                AddIngredient(ItemID.ChlorophyteBar, 6).
-                AddTile(TileID.MythrilAnvil).
-                Register();
-        }
+        public override int FishingPower => 40;
+        public override int BobberProjectile => ModContent.ProjectileType<ChlorophyteBobber>();
+        public override uint ProjectileCount => 2;
+        public override int Value => Item.buyPrice(gold: 3);
+        public override int Rarity => ItemRarityID.Lime;
+        public override int CraftingIngredient => ItemID.ChlorophyteBar;
+        public override int CraftingTile => TileID.MythrilAnvil;
     }
 }
