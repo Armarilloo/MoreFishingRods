3e7f8de256146e3d91cb42a95475036a2bd4aa64
Armarilloo <drekmarshal@gmail.com>
2025-07-28 00:05:53 -0400

    Added TitaniumFishingRod.cs and TitaniumBobber.cs

---
+++ Items/Fishing/FishingRods/TitaniumFishingRod.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using MoreFishingRods.Projectiles.Typeless;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreFishingRods.Items.Fishing.FishingRods;
+
+public class TitaniumFishingRod : BaseModFishingRod
+{
+    public override int FishingPower => 35;
+    public override int BobberProjectile => ModContent.ProjectileType<TitaniumBobber>();
+    public override int Value => Item.sellPrice(gold: 2, silver: 15);
+    public override int Rarity => ItemRarityID.LightRed;
+    public override int CraftingIngredient => ItemID.TitaniumBar;
+    public override int CraftingTile => TileID.MythrilAnvil;
+    public override Color LineColor => new Color(208, 208, 208, 1);
+}
