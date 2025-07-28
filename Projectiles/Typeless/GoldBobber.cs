c53414dc3d9fdeb1a8cb6407102ad66d787f262e
baer1 <videocreator@outlook.de>
2025-07-28 22:07:11 +0200

    Remove invisible glow effect from gold bobber

--- Projectiles/Typeless/GoldBobber.cs
+++ Projectiles/Typeless/GoldBobber.cs
@@ -1,12 +1,3 @@
-﻿using Terraria;
+﻿namespace MoreFishingRods.Projectiles.Typeless;
 
-namespace MoreFishingRods.Projectiles.Typeless;
-
-public class GoldBobber : BaseModBobber
-{
-    public override bool PreDrawExtras()
-    {
-        Lighting.AddLight(Projectile.Center, 0f, 0f, 0f);
-        return true;
-    }
-}
+public class GoldBobber : BaseModBobber { }
