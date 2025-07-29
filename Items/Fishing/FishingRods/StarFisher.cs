566a33d9cb866864aced2bf4650e87989d9e8bbc
Armarilloo <drekmarshal@gmail.com>
2025-07-28 20:39:35 -0400

    Fixed Tooltips Added StarFisher.cs Added MeteoriteBobber.cs

---
+++ Items/Fishing/FishingRods/StarFisher.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using MoreFishingRods.Projectiles.Typeless;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreFishingRods.Items.Fishing.FishingRods;
+
+public class StarFisher : BaseModFishingRod
+{
+    public override int FishingPower => 30;
+    public override int BobberProjectile => ModContent.ProjectileType<MeteoriteBobber>();
+    public override int Value => Item.sellPrice(gold: 1);
+    public override int Rarity => ItemRarityID.Blue;
+    public override int CraftingIngredient => ItemID.MeteoriteBar;
+    public override int CraftingTile => TileID.Anvils;
+    public override Color LineColor => new Color(252, 200, 119, 1);
+
+    public override bool CanFishInLava => true;
+}
---
+++ Projectiles/Typeless/MeteoriteBobber.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MoreFishingRods.Projectiles.Typeless;
+
+public class MeteoriteBobber : BaseModBobber
+{
+    public override Vector3? Glow => new Vector3(.25f, 0.15f, 0f);
+}
