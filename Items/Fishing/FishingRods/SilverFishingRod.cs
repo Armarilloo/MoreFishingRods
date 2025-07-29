144f1d191d07f1e5c63167a2638458155cfee552
Armarilloo <drekmarshal@gmail.com>
2025-07-29 12:33:32 -0400

    Added SilverFishingRod.cs Added SilverBobber.cs

---
+++ Items/Fishing/FishingRods/SilverFishingRod.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using MoreFishingRods.Projectiles.Typeless;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreFishingRods.Items.Fishing.FishingRods;
+
+public class SilverFishingRod : BaseModFishingRod
+{
+    public override int FishingPower => 16;
+    public override int BobberProjectile => ModContent.ProjectileType<SilverBobber>();
+    public override int Value => Item.sellPrice(silver: 50);
+    public override int Rarity => ItemRarityID.White;
+    public override int CraftingIngredient => ItemID.SilverBar;
+    public override int CraftingTile => TileID.Anvils;
+    public override Color LineColor => new Color(246, 249, 250, 1);
+}
---
+++ Projectiles/Typeless/SilverBobber.cs
@@ -0,0 +1,3 @@
+﻿namespace MoreFishingRods.Projectiles.Typeless;
+
+public class SilverBobber : BaseModBobber { }
