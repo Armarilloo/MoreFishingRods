e343a0b6bed62d8392ab31e6b0d6d9674738783f
Armarilloo <drekmarshal@gmail.com>
2025-07-28 22:26:43 -0400

    Added OrichalcumFishingRod.cs Added OrichalcumBobber.cs Fixed Tooltips

---
+++ Items/Fishing/FishingRods/OrichalcumFishingRod.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using MoreFishingRods.Projectiles.Typeless;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreFishingRods.Items.Fishing.FishingRods;
+
+public class OrichalcumFishingRod : BaseModFishingRod
+{
+    public override int FishingPower => 33;
+    public override int BobberProjectile => ModContent.ProjectileType<OrichalcumBobber>();
+    public override int Value => Item.sellPrice(gold: 2);
+    public override int Rarity => ItemRarityID.LightRed;
+    public override int CraftingIngredient => ItemID.OrichalcumBar;
+    public override int CraftingTile => TileID.MythrilAnvil;
+    public override Color LineColor => new Color(255, 241, 254, 1);
+
+    public override bool CanFishInLava => true;
+}
---
+++ Projectiles/Typeless/OrichalcumBobber.cs
@@ -0,0 +1,3 @@
+﻿namespace MoreFishingRods.Projectiles.Typeless;
+
+public class OrichalcumBobber : BaseModBobber { }
