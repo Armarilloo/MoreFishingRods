4c97097a486c21517a7cc446e0b729896da914d2
Armarilloo <drekmarshal@gmail.com>
2025-07-28 23:54:45 -0400

    Added PalladiumFishingRod.cs Added PalladiumBobber.cs

---
+++ Items/Fishing/FishingRods/PalladiumFishingRod.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using MoreFishingRods.Projectiles.Typeless;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreFishingRods.Items.Fishing.FishingRods;
+
+public class PalladiumFishingRod : BaseModFishingRod
+{
+    public override int FishingPower => 30;
+    public override int BobberProjectile => ModContent.ProjectileType<PalladiumBobber>();
+    public override int Value => Item.sellPrice(gold: 1, silver: 55);
+    public override int Rarity => ItemRarityID.LightRed;
+    public override int CraftingIngredient => ItemID.PalladiumBar;
+    public override int CraftingTile => TileID.Anvils;
+    public override Color LineColor => new Color(247, 214, 196, 1);
+
+    public override bool CanFishInLava => true;
+}
