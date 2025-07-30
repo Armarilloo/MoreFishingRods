db952ba45e2a3e01cb62a6a103f008f27aad10dd
Armarilloo <drekmarshal@gmail.com>
2025-07-30 17:47:12 -0400

    Added NebulaRod.cs Added NebulaBobber.cs
    Fixed SolarBobber.cs Glow color

---
+++ Projectiles/Typeless/NebulaBobber.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MoreFishingRods.Projectiles.Typeless;
+
+public class NebulaBobber : BaseModBobber
+{
+    public override Vector3? Glow => new Vector3(1f, 0f, 1f);
+}
--- Projectiles/Typeless/SolarBobber.cs
+++ Projectiles/Typeless/SolarBobber.cs
@@ -4,5 +4,5 @@ namespace MoreFishingRods.Projectiles.Typeless;
 
 public class SolarBobber : BaseModBobber
 {
-    public override Vector3? Glow => new Vector3(0f, 0.25f, 0f);
+    public override Vector3? Glow => new Vector3(1f, 0.64f, 0f);
 }
