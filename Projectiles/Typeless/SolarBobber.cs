727ca8c362d8610099f31950ffb0625f5a4b28f8
Armarilloo <drekmarshal@gmail.com>
2025-07-30 15:47:20 -0400

    Added SolarRod.cs Added SolarBobber.cs

---
+++ Projectiles/Typeless/SolarBobber.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MoreFishingRods.Projectiles.Typeless;
+
+public class SolarBobber : BaseModBobber
+{
+    public override Vector3? Glow => new Vector3(0f, 0.25f, 0f);
+}
